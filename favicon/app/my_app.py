14b8c139622dca703e1e6f5f8bb15a255e0891a8
yukht <yukht_dev@bk.ru>
2023-01-31 12:39:38 +0300

    Изменил readme

--- favicon/app/my_app.py
+++ favicon/app/my_app.py
@@ -3,7 +3,6 @@ arg = sys.argv
 if len(arg) != 3:
 	print("Enter two arguments: url and name of favicon")
 else:
-
 	url = str(arg[1])
 	filename = "/app/" + str(arg[2]) + "_favicon.ico"
 	r = requests.get(url, allow_redirects=True)
